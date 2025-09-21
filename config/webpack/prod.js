ac83fb9dff019a078e983cb20bf7a21af3571037
Oleg Apostol <truerenton@gmail.com>
2025-09-21 16:29:46 +0100

    fix: disable brotli

--- config/webpack/prod.js
+++ config/webpack/prod.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-const BrotliPlugin = require('brotli-webpack-plugin');
 const zopfli = require('@gfx/zopfli');
 const OfflinePlugin = require('@lcdp/offline-plugin');
 const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
@@ -40,12 +39,12 @@ module.exports = merge(common, {
         return zopfli.gzip(input, compressionOptions, callback);
       },
     }),
-    new BrotliPlugin({
-      asset: '[path].br[query]',
-      test: /\.(js|css|html|svg)$/,
-      threshold: 0,
-      minRatio: 1,
-    }),
+    // new BrotliPlugin({
+    //   asset: '[path].br[query]',
+    //   test: /\.(js|css|html|svg)$/,
+    //   threshold: 0,
+    //   minRatio: 1,
+    // }),
     new OfflinePlugin({
       ServiceWorker: {
         events: true,
